f6dd4b9b1a4dfe89065955d72d92f447d1a1b391
tiagocollot <collot.tiago1@gmail.com>
2023-06-29 13:35:08 +0100

    testing

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -1,19 +1,14 @@
 package com.example
-import com.project.SignUpUser
-import com.example.models.SignUpUserModel
 import com.example.models.LogInUserModel
+import com.example.models.SignUpUserModel
 import com.project.LoginUser
+import com.project.SignUpUser
 import org.http4k.core.*
-import org.http4k.lens.FormField
-import org.http4k.lens.Validator
-import org.http4k.lens.webForm
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
 import org.http4k.template.HandlebarsTemplates
-import org.http4k.template.ViewModel
-import java.util.*
 
 val templateRenderer = HandlebarsTemplates().HotReload("src/main/resources")
 
@@ -24,7 +19,7 @@ val app: HttpHandler = routes(
     "/" bind Method.GET to { _: Request ->
         Response(Status.OK).body("Hello")
     },
-    "/register-user" bind Method.GET to { request: Request ->
+    "/register-user" bind Method.GET to { _: Request ->
        /* val form = signUpUser.registrationForm(request)
         val username = signUpUser.requiredUser(form)
         val email = signUpUser.requiredEmail(form)
